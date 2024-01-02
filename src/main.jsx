c8aafdb42aa3691307371440382fac9aa165db04
GoitStudentsWorks <129747846+GoitStudentsWorks@users.noreply.github.com>
2024-01-02 21:56:28 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter basename="/HealthyHub">
+      <BrowserRouter basename="/fs-84-react-node-AnnaHort">
         <App />
       </BrowserRouter>
       </PersistGate>
