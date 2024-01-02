d01b5d2491710cd92aed012739c259352804354f
GoitStudentsWorks <129747846+GoitStudentsWorks@users.noreply.github.com>
2024-01-02 19:56:56 +0000

    Deploying to gh-pages from @ GoitStudentsWorks/fs-84-react-node-AnnaHort@f9d874b8a02dc1463394e3e8115dc8ccb9509e1f 🚀

---
+++ assets/DiaryPage-0f2a405f.js
@@ -0,0 +1,212 @@
+import{u as n,r as l,b as C,j as i,L as P,g as w,a4 as S,m as F}from"./index-487d118a.js";import{S as M,R as z,h as I,D as B,L,i as R}from"./RecordDiaryModal-212f7dd4.js";import"./formik.esm-cbf44169.js";import"./index.esm-d3e4f662.js";const N=n.div`
+  width: 300px;
+  margin-bottom: 40px;
+  @media (min-width: 834px) {
+    width: 780px;
+  }
+  @media (min-width: 1440px) {
+    width: 1440px;
+    display: flex;
+    flex-flow: row wrap;
+    margin-bottom: 52px;
+    margin-left: 0;
+    margin-right: 0;
+    gap: 26px 20px;
+  }
+`,A=n.h1`
+  color: var(--color-primary-white);
+  font-family: Poppins;
+  font-size: 24px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 30px;
+  margin-top: 16px;
+  margin-bottom: 16px;
+  display: flex;
+  @media (min-width: 834px) {
+    font-size: 30px;
+    font-weight: 500;
+    line-height: 36px;
+    margin-top: 24px;
+    margin-bottom: 16px;
+  }
+  @media (min-width: 1440px) {
+
+  }
+`,y=n.img`
+  width: 32px;
+  height: 32px;
+  margin-right: 12px;
+  @media (min-width: 1440px) {
+    margin: 0;
+    padding: 0;
+  }
+`,E=n.div`
+  @media (min-width: 834px) {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+`,U=n.h2`
+  color: var(--color-primary-white);
+  font-family: Poppins;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 26px;
+  display: flex;
+  margin-bottom: 12px;
+  @media (min-width: 834px) {
+    width: 155px;
+    font-size: 22px;
+    line-height: 32px;
+    margin-bottom: 6px;
+  }
+  @media (min-width: 1440px) {
+    gap: 12px;
+    display: inline-flex;
+    align-items: flex-start;
+  }
+`,O=n.div`
+  flex-direction: column;
+  display: flex;
+  flex-flow: row wrap;
+  @media (min-width: 834px) {
+    gap: 34px;
+    margin-right: 64px;
+  }
+  @media (min-width: 1440px) {
+    gap: 60px;
+  }
+`,s=n.p`
+  color: var(--color-primary-white);
+  font-family: Poppins;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 20px;
+  margin-right: 60px;
+  margin-bottom: 12px;
+  @media (min-width: 834px) {
+    margin: 0;
+  }
+  @media (min-width: 1440px) {
+    margin-right: 0;
+    margin-bottom: 0;
+  }
+`,_=n.div`
+  width: 300px;
+  height: 232px;
+  border-radius: 12px;
+  background: var(--color-primary-black-2);
+  margin-bottom: 24px;
+  @media (min-width: 834px) {
+    width: 780px;
+    height: 240px;
+  }
+  @media (min-width: 1440px) {
+    width: 676px;
+    margin: 0px;
+    padding-left: 20px;
+    margin-left: 0px;
+    margin-right: 20px;
+  }
+`,H=n.button`
+  color: var(--color-primary-green-lite);
+  background-color: var(--color-primary-black-2);
+  font-family: Poppins;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 20px;
+  border: none;
+  padding-bottom: 0px;
+  @media (min-width: 1440px) {
+  }
+`;n.p`
+  color: var(--color-primary-white);
+  font-family: Poppins;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 20px;
+  padding-left: 12px;
+  margin-bottom: 22px;
+  padding-top: 16px;
+  display: flex;
+
+  @media (min-width: 834px) {
+    /* padding-top: 32px;
+    padding-left: 14px; */
+  }
+`;const q=n.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 16px;
+  @media (min-width: 834px) {
+    flex-direction: row;
+    justify-content: space-between;
+  }
+`,G=n.div`
+  padding: 16px 12px;
+  @media (min-width: 834px) {
+    padding: 32px 14px;
+  }
+`,J=n.div`
+  display: flex;
+  @media (min-width: 834px) {
+  }
+`;n.img`
+  width: 16px;
+  height: 16px;
+  margin-left: 16px;
+  margin-right: 6px;
+  padding-bottom: 0px;
+`;const K=n.p`
+  color: var(--color-primary-white);
+  font-family: Poppins;
+  font-size: 14px;
+  font-weight: 600;
+  line-height: 1.43;
+  margin-right: 16px;
+  font-style: normal;
+`,Q=n.p`
+  color: var(--color-primary-white);
+  font-family: Poppins;
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 1.43;
+  font-style: normal;
+`,V=n.div`
+  display: flex;
+  gap: 12px;
+  margin-left: 22px;
+  @media (min-width: 834px) {
+    margin-left: 0px;
+    gap: 32px;
+  }
+`,c=n.span`
+  color: var(--color-primary-grey);
+  font-family: Poppins;
+  font-size: 12px;
+  font-weight: 400;
+  line-height: 1.5;
+  margin-right: 6px;
+  font-style: normal;
+  @media (min-width: 834px) {
+    display: none;
+  }
+`,m=n.p`
+  color: var(--color-primary-white);
+  font-family: Poppins;
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 1.5;
+  @media (min-width: 834px) {
+    width: 80px;
+  }
+`,W=n.button`
+  background-color: transparent;
+  border: none;
+`,X="/fs-84-react-node-AnnaHort/assets/arrow-back-3924980b.svg",Y=a=>{switch(a){case"breakfast":return R;case"lunch":return L;case"dinner":return B;case"snack":return I;default:return""}},g=(a,d)=>a.reduce((r,x)=>r+x[d],0),ni=()=>{const[a,d]=l.useState(),r=C(),[x,f]=l.useState(!1),[u,j]=l.useState("");if(l.useEffect(()=>{(async()=>{const e=await r(w());d(e.payload.food.foodServings)})()},[r]),!a)return null;const o=(t=>t.reduce((e,p)=>{const h=p.mealType.toLowerCase();return e[h]||(e[h]=[]),e[h].push(p),e},{}))(a),b=["snack","dinner","lunch","breakfast"],D=t=>{f(!0),j(t)},v=()=>{f(!1)},k=t=>{try{r(S(t))}catch(e){console.log(e)}finally{r(w()),r(F())}};return i.jsxs(i.Fragment,{children:[i.jsx(P,{to:"/main",children:i.jsxs(A,{children:[i.jsx(y,{src:X,alt:"#"}),"Diary"]})}),i.jsxs(N,{children:[b.map(t=>i.jsxs("div",{children:[i.jsxs(E,{children:[i.jsxs(U,{children:[i.jsx(y,{src:Y(t),alt:"#"}),t.charAt(0).toUpperCase()+t.slice(1)]}),i.jsx(O,{children:o[t]&&o[t].length>0?i.jsxs(i.Fragment,{children:[i.jsxs(s,{children:["Carbonohidrates:",g(o[t],"carbonohidrates")]}),i.jsxs(s,{children:["Protein:",g(o[t],"protein")]}),i.jsxs(s,{children:["Fat:",g(o[t],"fat")]})]}):i.jsxs(i.Fragment,{children:[i.jsx(s,{children:"Carbonohidrates: 0"}),i.jsx(s,{children:"Protein: 0"}),i.jsx(s,{children:"Fat: 0"})]})})]}),i.jsxs(_,{children:[i.jsx(G,{children:o[t]&&o[t].length>0?o[t].map((e,p)=>i.jsxs(q,{children:[i.jsxs(J,{children:[i.jsx(K,{children:p+1}),i.jsx(Q,{children:e.dishName})]}),i.jsxs(V,{children:[i.jsxs(m,{children:[i.jsx(c,{children:"Carb."}),e.carbonohidrates]}),i.jsxs(m,{children:[i.jsx(c,{children:"Prot."}),e.protein]}),i.jsxs(m,{children:[i.jsx(c,{children:"Fat."}),e.fat]}),i.jsx(W,{onClick:()=>k(e._id),children:i.jsx(M,{})})]})]},e._id)):null}),i.jsx(H,{onClick:()=>D(t),children:"+ Record your meal"})]})]},t)),x&&i.jsx(z,{onClose:v,selectedMeal:u})]})]})};export{ni as default};
---
+++ assets/ForgotPasswordPage-cf7e0701.js
@@ -0,0 +1,109 @@
+import{u as e,L as l,d as a,j as i,k as m,Q as r}from"./index-487d118a.js";import{S as p,R as d,F as g,e as h}from"./SignInPages.styled-36f9000a.js";import{S as c}from"./SignUpRegister.styled-13b769a8.js";import{c as x,a as u}from"./index.esm-d3e4f662.js";import{u as f}from"./formik.esm-cbf44169.js";const w=e.div`
+  display: flex;
+  flex-direction: column;
+  gap: 56px;
+  @media (min-width: 834px) {
+    margin-right: auto;
+    margin-left: auto;
+    width: 380px;
+    gap: 286px;
+  }
+  @media (min-width: 1440px) {
+      margin-right: 0;
+      margin-left: 0;
+    }
+`,y=e.h2`
+  color: var(--color-primary-white);
+  font-family: Poppins;
+  font-size: 24px;
+  font-weight: 500;
+  line-height: 1.25;
+  margin-bottom: 16px;
+  @media (min-width: 834px) {
+    font-size: 30px;
+    line-height: 1.2;
+    text-align: center;
+    margin-bottom: 16px;
+    @media (min-width: 1440px) {
+      text-align: start;
+    }
+  }
+`,v=e.p`
+  color: var(--color-primary-grey);
+  font-family: Poppins;
+  font-size: 18px;
+  line-height: 1.33;
+  margin-bottom: 24px;
+  @media (min-width: 834px) {
+    font-size: 22px;
+    line-height: 1.45;
+  }
+  @media (min-width: 1440px) {
+      text-align: start;
+    }
+`,b=e.form`
+  @media (min-width: 834px) {
+    margin-left: auto;
+    margin-right: auto;
+  }
+
+  @media (min-width: 1440px) {
+    margin-left: 0;
+    margin-right: 0;
+    text-align: start;
+  }
+`,j=e.input`
+  color: var(--color-primary-grey);
+  font-family: Poppins;
+  line-height: 1.429;
+  outline: none;
+  padding: 8px 10px;
+  width: 100%;
+  background: transparent;
+  position: relative;
+  border: 1px solid var(--color-primary-green-lite);
+  border-radius: 12px;
+  max-width: 380px;
+  margin-bottom: 20px;
+  margin-left: auto;
+  margin-right: auto;
+
+  :hover {
+    color: var(--color-primary-white);
+  }
+  @media (min-width: 1440px) {
+    display: block;
+    max-width: 212px;
+    margin-left: 0;
+    margin-right: 0;
+  }
+`,F=e.button`
+  border-radius: 12px;
+  border: none;
+  outline: none;
+  background: var(--color-primary-green-lite);
+  padding: 8px 10px;
+  margin-top: 20px;
+  color: var(--bg-primary);
+  text-align: center;
+  font-weight: 500;
+  line-height: 1.43;
+  width: 100%;
+`,P=e.div`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  @media (min-width: 834px) {
+    flex-direction: row;
+  }
+`,S=e.p`
+  color: var(--color-primary-grey);
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 1.43;
+`,k=e(l)`
+  color: var(--color-primary-white);
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 1.43;
+`;a.defaults.baseURL="https://healthhub-backend.onrender.com";const C=/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,z=x().shape({email:u().matches(C,"Invalid email").required("Email is required")}),E=()=>{const t=f({initialValues:{email:""},validationSchema:z,onSubmit:async(n,{resetForm:s})=>{try{const o=await a.post("/api/auth/forgot-password",n);r.success(o.data.message,{autoClose:3e3}),s()}catch(o){r.error(o.response.data.message,{autoClose:2e3})}}});return i.jsxs(p,{children:[i.jsx(c,{viewBox:"0 0 300 296"}),i.jsxs(d,{children:[i.jsx(y,{children:"Forgot your password"}),i.jsx(v,{children:"We will send you an email with recovery instructions"}),i.jsxs(b,{autoComplete:"off",onSubmit:t.handleSubmit,children:[i.jsxs("div",{style:{position:"relative"},children:[i.jsx("label",{htmlFor:"email",children:i.jsx(j,{type:"email",id:"email",name:"email",placeholder:"E-mail",onChange:t.handleChange,onBlur:t.handleBlur,value:t.values.email})}),t.touched.email&&t.errors.email&&i.jsx(g,{children:t.errors.email})]}),i.jsxs(w,{children:[i.jsx(h,{children:i.jsx(F,{type:"submit",children:"Send"})}),i.jsxs(P,{children:[i.jsx(S,{children:"If you don't have an account yet"}),i.jsx(k,{to:"/signup",children:"Sign up"})]})]})]})]}),i.jsx(m,{position:"top-right"})]})};export{E as default};
